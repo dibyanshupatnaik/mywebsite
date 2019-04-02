cbcec8e9886555fb10c325121aa2feba51aceb64
Harsh Praharaj <praharajhp@gmail.com>
2019-04-02 22:44:44 +0530

    Add files via upload

---
+++ dist/js/main.js
@@ -0,0 +1,173 @@
+// when google maps api gets failed
+var googleError = function(){
+    self.error_message('Failed to load GoogleMaps Api');
+    self.apiError(true);
+};
+// when google maps api gets failed
+var FourSquareError = function(){
+    self.error_message('Failed to load Foursquare Api');
+    self.apiError(true);
+};
+
+var map ;
+this.marker;
+var initMap = function(){
+    var pyrmont = {
+            lat: 28.5932853,
+            lng: 77.2485266
+        };
+    // displays the requested map content in map div 
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: pyrmont,
+        zoom: 3
+    });
+    // displays location information in window when marker clicked
+    infowindow = new google.maps.InfoWindow();
+    
+              
+    for(var i=0; i<places.length; i++){
+        AddMarker(places[i]);
+    }
+  
+};
+
+
+// Adds the marker by getting place location details
+var AddMarker = function(place){
+        var myLatLng = { lat:place.location.lat,
+                         lng:place.location.lng };
+        self.marker = new google.maps.Marker({
+            map:map,
+            animation: google.maps.Animation.DROP,
+            position: myLatLng
+        });
+        if(self.marker){
+            self.markersArray().push([myLatLng,
+                                     self.marker]);                    
+            google.maps.event.addListener(marker, 'click', function() {
+                stopAnimation();
+                startAnimation(myLatLng);
+                FoursquareData(place);
+            });
+        }
+};
+
+// removes all the markers
+var removeMarkers = function(){
+    for(var x=0; x<self.markersArray().length; x++ ){
+        self.markersArray()[x][1].setMap(null);
+    }
+};
+// shows all the markers    
+var showMarkers = function(){
+    for(var i=0; i<self.markersArray().length; i++ ){
+        self.markersArray()[i][1].setMap(map);
+    }
+}; 
+
+// starts the marker bounce animation
+var startAnimation = function(myLatLng){
+    ko.computed(function(){
+            ko.utils.arrayForEach(self.markersArray(), function(m){
+                if(myLatLng.lat === m[0].lat && myLatLng.lng ===m[0].lng){
+                    m[1].setAnimation(google.maps.Animation.BOUNCE);
+                }
+            });
+        });
+};
+
+// stops the marker bounce animation
+var stopAnimation = function(){
+    for(var i=0; i<self.markersArray().length; i++ ){
+        self.markersArray()[i][1].setAnimation(null);
+    }
+};
+
+// Gets the location data from Foursquare
+var FoursquareData = function(place){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; 
+    var yyyy = today.getFullYear();
+    
+    if(dd<10){
+        dd='0'+dd;
+    } 
+    if(mm<10){
+        mm='0'+mm;
+    } 
+    today = ""+yyyy+mm+dd+"";
+
+    var venue_id = place.venue_id;
+    var client_id = "ZG0FZ2ZJ00NVTZEK2YLORO50HYJGO1XTFCBN2KTVYWN2Z2SV";
+    var client_secret = "YNNEH4MXM50R04LNNDMESQIMRZL5XOVN1ITGA3K4CU2E2KLR";
+    var FoursquareUrl = "https://api.foursquare.com/v2/venues/"+venue_id+"?client_id="+client_id+"&client_secret="+client_secret+"&v="+today+"" ;
+    
+    $.ajax({
+        url:FoursquareUrl,
+        dataType:"json",
+        async:true        
+    }).success(function(data){
+            self.des_name(data.response.venue.name);
+            self.rating(data.response.venue.rating);
+            var image_prefix = data.response.venue.bestPhoto.prefix;
+            var image_suffix = data.response.venue.bestPhoto.suffix;
+            self.location_image(image_prefix +"320x200"+ image_suffix);
+            if(data.response.venue.tips.groups[0].items[0].text){
+                self.review(data.response.venue.tips.groups[0].items[0].text);
+            }   
+    }).error(function(data){
+        FourSquareError();
+    });
+    
+};
+
+
+
+
+
+var viewModel = function(){
+    var self = this;
+    this.markersArray = ko.observableArray([]);
+    this.query = ko.observable();
+    this.location_image = ko.observable();
+    this.des_name = ko.observable();
+    this.rating = ko.observable();
+    this.review = ko.observable();
+    this.apiError = ko.observable(false);
+    this.error_message = ko.observable();
+    // filters the places array when searched in a query input
+    this.searchResults = ko.computed(function() {
+        q = self.query();
+        if(!q){
+            showMarkers();
+            return places;
+        }
+        else{
+            removeMarkers();
+            return ko.utils.arrayFilter(places, function(place) {
+                if(place.name.toLowerCase().indexOf(q) >= 0) {
+                    AddMarker(place);
+                    return place;
+                }    
+            });
+        }
+    });
+
+    // when name of the location clicked displays infowindow
+    this.viewPlace = function(place){
+        var check_myLatLng = {lat:place.location.lat,
+                               lng:place.location.lng};
+        stopAnimation();
+        startAnimation(check_myLatLng);
+        FoursquareData(place);       
+    };  
+    this.closeDesc = function(){
+        self.des_name(null);
+    };
+     
+};     
+
+  
+    
+ko.applyBindings(viewModel);  
---
+++ dist/js/model.js
@@ -0,0 +1,188 @@
+var places = [
+    {
+        "name": "Rashtrapati Bhawan",
+        "venue_id": "4d57c17fafe4b60c84784861",
+        "location" : {
+            "lat": 28.61436,
+            "lng":77.1974323
+        }
+    },
+    {
+        "name": "Qutub Minar",
+        "venue_id": "4ba47c9bf964a520fe9f38e3",
+        "location" : {
+            "lat": 28.5244281,
+            "lng": 77.1832672
+        }
+    },
+    {
+        "name": "India Gate",
+        "venue_id": "4b5eeab3f964a520ca9d29e3",
+        "location" : {
+            "lat": 28.612912,
+            "lng": 77.227321
+        }
+    },
+    {
+        "name": "Humayun's Tomb",
+        "venue_id": "4bf7829cb182c9b6f2f0755a",
+        "location" : {
+            "lat": 28.5932853,
+            "lng": 77.2485266
+        }
+    },
+    {
+        "name": "Red Fort",
+        "venue_id": "4bf214b83506ef3b1d7abd22",
+        "location" : {
+            "lat": 28.6561592,
+            "lng": 77.2388316
+        }
+    },
+    {
+        "name": "Ugrasen ki Baoli",
+        "venue_id":"4e070cdad22d658532a39289",
+        "location" : {
+            "lat": 28.6261132,
+            "lng": 77.2228093
+        }
+    },
+    {
+        "name": "Lotus Temple",
+        "venue_id": "4ba48dc3f964a520e2a338e3",
+        "location" : {
+            "lat": 28.553492,
+            "lng": 77.2566377
+        }
+    },
+    {
+        "name": "IJburg volgens mij.",
+        "venue_id": "524d856811d26e06a424282f",
+        "location" : {
+            "lat": 52.356361,
+            "lng": 4.997021
+        }
+    },
+    {
+        "name": "Victory Outreach",
+        "venue_id": "4cb9cfa6bac937048705e77c",
+        "location" : {
+            "lat": 52.337490223602,
+            "lng": 4.9280156900214
+        }
+    },
+    {
+        "name": "Pocahondas",
+        "venue_id": "5206001f11d20dff26312085",
+        "location" : {
+            "lat": 52.370207010005,
+            "lng": 4.7778619313434
+        }
+    },
+    {
+        "name": "Diemerpark",
+        "venue_id": "4bd40042046076b014457771",
+        "location" : {
+            "lat": 52.352236531042,
+            "lng": 4.987964630127
+        }
+    },
+    {
+        "name": "Zeeburg",
+        "venue_id": "50734031e4b06d8f8ac836b4",
+        "location" : {
+            "lat": 52.358934,
+            "lng": 4.996342
+        }
+    },
+    {
+        "name": "Theo van Goghpark",
+        "venue_id": "4bd98b2f0115c9b692a07780",
+        "location" : {
+            "lat": 52.350178768514,
+            "lng": 5.0053882598877
+        }
+    },
+    {
+        "name": "Palacio Del Flamenco",
+        "venue_id": "4adcda4ff964a520aa4021e3",
+        "location" : {
+            "lat": 41.393372031415,
+            "lng": 2.1567348634482
+        }
+    },
+    {
+        "name": "Museo del Real Monasterio de Santa María de Pedralbes",
+        "venue_id": "4adcda50f964a520314121e3",
+        "location" : {
+            "lat": 41.396082894092,
+            "lng": 2.1125043384588
+        }
+    },
+    {
+        "name": "Parc de Collserola",
+        "venue_id": "4adcda50f964a520514121e3",
+        "location" : {
+            "lat":  41.420943563881,
+            "lng": 2.0995473861694
+        }
+    },
+    {
+        "name": "Camp Nou",
+        "venue_id": "507fe73fd63e93639fd2a84f",
+        "location" : {
+            "lat": 41.380817685705,
+            "lng": 2.1228504180908  
+        }
+    },
+    {
+        "name": "La Sagrada Familia",
+        "venue_id": "4ca61fcff47ea1437e765d21",
+        "location" : {
+            "lat": 41.41124312409,
+            "lng": 2.1749239621508 
+        }
+    },
+    {
+        "name": "The Berlin Wall",
+        "venue_id": "4c2f5bb216adc928c65bbd9c",
+        "location" : {
+            "lat": 52.50397982974,
+            "lng": 13.442703900567
+        }
+    },
+    {
+        "name": "Reichstagskuppel",
+        "venue_id": "4c99d25c292a6dcb0c45c976",
+        "location" : {
+            "lat": 52.518636487406,
+            "lng": 13.376047611237
+        }
+    },
+    {
+        "name": "FC Borussia Pankow 1960 e.V.",
+        "venue_id": "4cf23315e9425481be006fc5",
+        "location" : {
+            "lat": 52.567277016861,
+            "lng": 13.394697073836
+        }
+    },
+    {
+        "name": "Hertha BSC ",
+        "venue_id": "4adcda92f964a520fb4b21e3",
+        "location" : {
+            "lat": 52.51883209166,
+            "lng": 13.243625137805
+        }
+    },
+    {
+        "name": "Memorial to the Murdered Jews of Europe ",
+        "venue_id": "4b10f428f964a520f77623e3",
+        "location" : {
+            "lat": 52.513870400102,
+            "lng": 13.378601074219
+        }
+    },
+
+
+]
